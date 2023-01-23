00fcd8ef0072175504b56286e588f05ea4335ed9
GurraNasan <gurra.naslund@gmail.com>
2023-01-23 15:52:29 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MTRe6FxabX2icHzmuEvA3nLRoxn5W79wupZuQeRu8Dh4l5skv4bKlohiHUsQ6aEPXvARQPfQygVUEPUDilNuvDy00Z7Jc1OEQ',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
