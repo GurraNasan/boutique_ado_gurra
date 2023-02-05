8e1b0fab2040e108424f2dd6d0166125708ba6d8
GurraNasan <gurra.naslund@gmail.com>
2023-02-05 11:55:17 +0000

    Add cache control

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -176,6 +176,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # AWS
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'THU, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=9999999',
+    }
+
     AWS_STORAGE_BUCKET_NAME = 'butikado'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
