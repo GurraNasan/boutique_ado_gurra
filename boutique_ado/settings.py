441b055c14acb4d67649a6e86a78fe00a9890193
GurraNasan <gurra.naslund@gmail.com>
2023-02-08 10:36:05 +0000

    add 404 handler

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -75,7 +75,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', # required by allauth
+                'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
@@ -126,8 +126,6 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
-
 
 
 # Password validation
